fa47b80eec1bd83531051cd750c7f91ee7043199
maxhaesslein <mail@maxhaesslein.de>
2017-10-17 18:12:16 +0200

    use right gui as compose key

--- keyboards/planck/keymaps/hex/keymap.c
+++ keyboards/planck/keymaps/hex/keymap.c
@@ -44,14 +44,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * |   \  |   ;  |   Q  |   J  |   K  |   X  |   B  |   M  |   W  |   V  |   Z  |  /   |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Ctrl |  GUI | Alt  | Comp |Lower |    Space    |Raise | Left | Down |  Up  |Right |
+ * | Ctrl |  GUI | Alt  | Comp |Lower |    Space    |Raise | Left | Down |  Up  |Right | (use RIGHT GUI as compose key)
  * `-----------------------------------------------------------------------------------'
  */
 [_DVORAK] = {
   {KC_TAB,  KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,    KC_F,    KC_G,    KC_C,    KC_R,    KC_L,    KC_BSPC},
   {F(0),    KC_A,    KC_O,    KC_E,    KC_U,    KC_I,    KC_D,    KC_H,    KC_T,    KC_N,    KC_S,    KC_ENT},   /* F(0): ESCAPE on press and SHIFT on hold */
   {KC_BSLS, KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,    KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,    KC_SLASH},
-  {KC_LCTL, KC_LGUI, KC_LALT, KC_PLUS, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT}   /* TODO: replace KC_PLUS with COMPOSE */
+  {KC_LCTL, KC_LGUI, KC_LALT, KC_RGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT}
 },
 
 /* Lower
